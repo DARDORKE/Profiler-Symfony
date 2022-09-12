57aa1f42cb99d8dc3b59cc8b2767ef1a9ff02d15
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-12 17:26:15 +0200

    Le pack Debug 2/2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/', name: 'app_default')]
-    public function index(Request $request): Response
+    #[Route("/", name: "default")]
+    // 1
+    public function index(LoggerInterface $logger): Response
     {
-        return dump($this)->render('default/index.html.twig', [
+        // 2
+        $logger->info('This is my log message.');
+
+        return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
