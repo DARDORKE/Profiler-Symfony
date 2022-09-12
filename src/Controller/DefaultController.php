67704bac39b7f1f84dc7760b87585aaab971e10a
Kévy DARDOR <kevy.dardor@gmail.com>
2022-09-12 17:11:06 +0200

    Débugger avec les fonctions dump() et dd() 2/2

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_default')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request);
+
+        dump($request->headers->all());
+
+        dd($request->headers->get('host'));
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
